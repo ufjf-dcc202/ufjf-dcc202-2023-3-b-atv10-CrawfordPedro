74eb448e3cc2f927d06536069e71b292e5ec62c8
Pedro Crawford <pedro.crawford@estudante.ufjf.br>
2023-12-04 17:44:05 -0300

    incluir main em src

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import {deJoaoParaMaria, deMariaParaJoao, getJoao, getMaria, setJoao, setMaria, joao, maria} from "./joaoEMaria.js"
+
+getJoao();
+
+setJoao(4);
+
+deJoaoParaMaria();
+
+getJoao();
+getMaria();
